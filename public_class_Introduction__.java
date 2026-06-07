80cf33de76419b98ad633e8c6a371340e86644ad
LiuXinh <liungu2005@gmail.com>
2026-06-07 13:44:25 +0700

    clean project

--- public_class_Introduction__.java
+++
@@ -1,7 +0,0 @@
-public class Introduction {
-    public static void main(String[] args) {
-        String name = "Ngoc Lan";
-        System.out.println("My name is " + name + ".");
-        
-    }
-}
