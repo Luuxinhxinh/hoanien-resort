8da7b8be088fb70fc5eb9b33cf35741d52504da7
LiuXinh <liungu2005@gmail.com>
2026-07-16 01:48:15 +0700

    abc

--- 05-Development/kawai-backend/src/main/java/com/kawai/services/interfaces/HousekeepingService.java
+++ 05-Development/kawai-backend/src/main/java/com/kawai/services/interfaces/HousekeepingService.java
@@ -60,4 +60,6 @@ public interface HousekeepingService {
      * @return Room phòng đã sẵn sàng
      */
     Room completeMaintenance(Long taskId);
+
+    void createMaintenanceTaskForPricedDamages(Room room);
 }
